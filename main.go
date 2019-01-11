4af9aaa1fac6506dc1c19313598d01591be84da4
Austin Millan <austin.millan@gmail.com>
2019-01-10 19:34:56 -0800

    Added example with multiple args

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/sbinet/go-python"
 )
 
+var PyStr = python.PyString_FromString
+
 
 func import_and_call_func() {
 	funcsModule := python.PyImport_ImportModule("funcs")
@@ -23,6 +25,7 @@ func import_and_call_func() {
 }
 
 func import_and_use_obj() {
+	// Import class from module
 	objsModule := python.PyImport_ImportModule("objs")
 	if objsModule == nil { panic("Error importing module: objs") }
 	fmt.Printf("[MODULE REF] repr(objsModule) = %s\n", python.PyString_AS_STRING(objsModule.Repr()))
@@ -31,13 +34,33 @@ func import_and_use_obj() {
 	if obj == nil { panic("[CLASS REF] Error importing object: obj") }
 	fmt.Printf("[CLASS REF] repr(obj) = %s\n", python.PyString_AS_STRING(obj.Repr()))
 
+	// Instantiate obj
 	newObj := python.PyInstance_New(obj, python.PyTuple_New(0), nil)
 	if newObj == nil { panic("[INSTANCE REF] Error instantiating object: obj") }
 	fmt.Printf("[INSTANCE REF] repr(newObj) = %s\n", python.PyString_AS_STRING(newObj.Repr()))
 
+	// Call method (no arguments)
 	methodCall := newObj.CallMethod("obj_method")
 	if newObj == nil { panic("[METHOD CALL REF] Error calling object method: obj_method") }
 	fmt.Printf("[METHOD CALL REF] repr(methodCall) return value = %s\n", python.PyString_AS_STRING(methodCall.Repr()))
+
+	// Create Python tuple and call method with tuple as arg
+	methodArg := python.PyTuple_New(1)
+	err := python.PyTuple_SetItem(methodArg, 0,  PyStr("FooArg"))
+	if err != nil { panic("[NEW TUPLE REF] Error setting item in tuple: methodArg") }
+	methodCallArg := newObj.CallMethodObjArgs("obj_method_arg", methodArg)
+	if methodCallArg == nil { panic("[METHOD CALL REF] Error calling object method: obj_method_arg") }
+	fmt.Printf("[METHOD CALL REF] repr(methodCallArg) return value = %s\n", python.PyString_AS_STRING(methodCallArg.Repr()))
+
+	// Now try with multiple args
+	methodArgTwo := python.PyList_New(2)
+	err = python.PyList_SetItem(methodArgTwo, 0,  PyStr("ListElement0"))
+	if err != nil { panic("[LIST SET REF] Error setting element at list[0]: methodArgTwo") }
+	err = python.PyList_SetItem(methodArgTwo, 1,  PyStr("ListElement1"))
+	if err != nil { panic("[LIST SET REF] Error setting element at list[1]: methodArgTwo") }
+	methodCallArgs := newObj.CallMethodObjArgs("obj_method_args", methodArg, methodArgTwo)
+	if methodCallArgs == nil { panic("[METHOD CALL REF] Error calling object method: obj_method_args") }
+	fmt.Printf("[METHOD CALL REF] repr(methodCallArgs) return value = %s\n", python.PyString_AS_STRING(methodCallArgs.Repr()))
 }
 
 
--- objs.py
+++ objs.py
@@ -6,3 +6,20 @@ class obj:
     def obj_method(self):
         print("Python object method called!")
         return 1
+
+    def obj_method_arg(self, arg1):
+        print("Python object method called, with argument: %s" % arg1)
+        return 1
+
+    def obj_method_args(self, arg1, arg2):
+        print("Python object method called with arguments: %s" % arg1, arg2)
+        return 1
+
+    def obj_method_kwargs(self, **kwargs, args):
+        print("Python object method called with arguments:" )
+        print(kwargs)
+        return 1
+#
+# if __name__ == '__main__':
+#     obj_a = obj()
+#     obj_a.obj_method_kwargs(**{'key': 'val'})
